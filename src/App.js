7ad9440141e999eb4d561420357979001ebb8d55
NadiaBCarmon <nadia.prysmmagazine@gmail.com>
2017-12-05 11:45:25 -0500

    Added Address Book assignment

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 }
 
 App.defaultProps = {
-	addresses: [<br />, <br />, 'O-Ren Ishii, +8174-788-1012, 191-1014, Nishishinjuku Shinjuku Nomurabiru(45-kai), Shinjuku-ku, Tokyo,', <br />, 'Vernita Green, (323)-280-4794, 1768 Norman Street, Los Angeles, CA 90017,', <br />, 'Budd, (626)-292-1202, 187 Fulsom Avenue, San Gabriel, CA 91775,', <br />, 'Elle Driver, (949)-569-4371, 1 East Bayberry Street, Hanford, CA 93230,', <br />, 'Bill, (413)-512-7496, 582 Creek Lane, El Cajon, CA 92021']
+	addresses: [ <br />, 'O-Ren Ishii, +8174-788-1012, 191-1014, Nishishinjuku Shinjuku Nomurabiru(45-kai), Shinjuku-ku, Tokyo,', <br />, 'Vernita Green, (323)-280-4794, 1768 Norman Street, Los Angeles, CA 90017,', <br />, 'Budd, (626)-292-1202, 187 Fulsom Avenue, San Gabriel, CA 91775,', <br />, 'Elle Driver, (949)-569-4371, 1 East Bayberry Street, Hanford, CA 93230,', <br />, 'Bill, (413)-512-7496, 582 Creek Lane, El Cajon, CA 92021']
 }
 
 
--- src/TextAndSubmit.js
+++ src/TextAndSubmit.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.js';
+import './Layout.css';
 
 class TextAndSubmit extends React.Component {
 
@@ -18,21 +19,23 @@ class TextAndSubmit extends React.Component {
 		let email = this.refs.email.value;
 		let phone = this.refs.phone.value;
 		let address = this.refs.address.value;
+		console.log(this);
+		//this.state({
+			//search: '',
+			//contacts: this.state.contacts.concat({name, email, phone, address})
+		//})
 
-		document.write(name + ' ' + email + ' ' + phone  + ' ' + address)
-		this.setState({
-					//contacts: this.state.contacts.map({name, email, phone, address})
-		})
 	}
+
+
 
 	render() {
 
 		//let filteredContacts = this.state.contacts.filter(
-		//	(contact) => {
-		//	return concat.contacts.toLowerCase().indexOf(this.state.search)
+			//(contact) => {
+		//return concat.contacts.toLowerCase().indexOf(this.state.search)
 			
-		//	}	
-		//);
+			//});
 
 			//<h2>Button to remove last entry</h2>
 
